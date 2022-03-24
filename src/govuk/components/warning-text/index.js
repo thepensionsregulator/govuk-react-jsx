a372c46c4b9246a6aa0ac002cb13b641e0408dd4
Rick Mason <rick.mason@tpr.gov.uk>
2022-03-24 11:59:13 +0000

    Update warning text component to support injected class names for CSS modules #97696

--- src/govuk/components/warning-text/index.js
+++ src/govuk/components/warning-text/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
 function WarningText(props) {
-  const { className, iconFallbackText, children, ...attributes } = props;
+  const {
+    govukClassNames,
+    className,
+    iconFallbackText,
+    children,
+    ...attributes
+  } = props;
+	const classNames = govukClassNames || {};
+  classNames['govuk-warning-text'] = classNames['govuk-warning-text'] || 'govuk-warning-text';
+  classNames['govuk-warning-text__icon'] = classNames['govuk-warning-text__icon'] || 'govuk-warning-text__icon';
+  classNames['govuk-warning-text__text'] = classNames['govuk-warning-text__text'] || 'govuk-warning-text__text';
+  classNames['govuk-warning-text__assistive'] = classNames['govuk-warning-text__assistive'] || 'govuk-warning-text__assistive';
 
   return (
-    <div className={`govuk-warning-text ${className || ''}`} {...attributes}>
-      <span className="govuk-warning-text__icon" aria-hidden="true">
+    <div className={`${classNames['govuk-warning-text']} ${className || ''}`} {...attributes}>
+      <span className={classNames['govuk-warning-text__icon']} aria-hidden="true">
         !
       </span>
-      <strong className="govuk-warning-text__text">
-        <span className="govuk-warning-text__assistive">
+      <strong className={classNames['govuk-warning-text__text']}>
+        <span className={classNames['govuk-warning-text__assistive']}>
           {iconFallbackText}
         </span>
         {children}
